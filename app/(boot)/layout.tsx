ddf8ff018549457e6561f7143e172a5d99f795d0
Igor Strelnyk <59284261+rodriguezz4060@users.noreply.github.com>
2024-09-25 16:20:53 +0300

    Update layout.tsx

--- app/(boot)/layout.tsx
+++ app/(boot)/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: 'RodriguezDB | Главная',
+  title: 'RodriguezDB | Подбор пыльников',
   description: 'База данных Родригеза',
 }
 
