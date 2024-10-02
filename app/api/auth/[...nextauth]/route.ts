03f1ec894db1b07fe50db25ca3d29b73949540b7
Igor Strelnyk <59284261+rodriguezz4060@users.noreply.github.com>
2024-10-02 16:55:08 +0300

    auth github
    in proces

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,13 +2,24 @@ import NextAuth from 'next-auth'
 import GitHubProvider from 'next-auth/providers/github'
 import CredentialsProvider from 'next-auth/providers/credentials'
 import { prisma } from '@/prisma/prisma-client'
-import { compare } from 'bcrypt'
+import { compare, hashSync } from 'bcrypt'
+import { UserRole } from '@prisma/client'
 
 export const authOptions = {
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID || '',
       clientSecret: process.env.GITHUB_SECRET || '',
+
+      profile(profile) {
+        return {
+          id: profile.id,
+          name: profile.name || profile.login,
+          email: profile.email,
+          image: profile.avatar_url,
+          role: 'USER' as UserRole,
+        }
+      },
     }),
     CredentialsProvider({
       name: 'Credentials',
@@ -53,6 +64,56 @@ export const authOptions = {
     strategy: 'jwt',
   },
   callbacks: {
+    async singIn({ user, account }) {
+      try {
+        if (account.provider === 'credentials') {
+          return true
+        }
+
+        if (!user.email) {
+          return false
+        }
+
+        const findUser = await prisma.user.findFirst({
+          where: {
+            OR: [
+              { provider: account?.provider, providerId: account?.providerAccountId },
+              { email: user.email },
+            ],
+          },
+        })
+
+        if (findUser) {
+          await prisma.user.update({
+            where: {
+              id: findUser.id,
+            },
+            data: {
+              provider: account?.provider,
+              providerId: account?.providerAccountId,
+            },
+          })
+
+          return true
+        }
+
+        await prisma.user.create({
+          data: {
+            email: user.email,
+            fullName: user.name || 'User #' + user.id,
+            password: hashSync(user.id.toString(), 10),
+            provider: account?.provider,
+            providerId: account?.providerAccountId,
+          },
+        })
+
+        return true
+      } catch (error) {
+        console.error('Error [SINGIN]', error)
+        return false
+      }
+    },
+
     async jwt({ token }) {
       const findUser = await prisma.user.findFirst({
         where: {
