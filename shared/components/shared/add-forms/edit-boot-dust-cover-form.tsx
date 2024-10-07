be7b5489520006cfaab5cdde3023faf497f1e3fc
Igor Strelnyk <59284261+rodriguezz4060@users.noreply.github.com>
2024-10-07 16:57:37 +0300

    add

--- shared/components/shared/add-forms/edit-boot-dust-cover-form.tsx
+++ shared/components/shared/add-forms/edit-boot-dust-cover-form.tsx
@@ -19,6 +19,8 @@ import { useIntl } from 'react-intl'
 import { BootWithCar } from '@/@types/prisma'
 import { TableColumns } from '@/shared/constants/table'
 import { EditBootModal } from '../modals'
+import { Car } from '@prisma/client'
+import { getCars } from '@/shared/services/cars'
 
 interface Props {
   data: BootWithCar
@@ -48,19 +50,22 @@ export const EditBootDustCoverForm: React.FC<Props> = ({ data, className }) => {
   const [names, setNames] = useState<{ id: number; name: string }[]>([])
   const [forms, setForms] = useState<{ id: number; form: string }[]>([])
   const [types, setTypes] = useState<{ id: number; type: string }[]>([])
+  const [BootWithCar, setBootWithCar] = useState<Car[]>([])
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const [names, types, forms] = await Promise.all([
+        const [names, types, forms, BootWithCar] = await Promise.all([
           getBootDustName(),
           getBootDustType(),
           getBootDustForm(),
+          getCars(),
         ])
 
         setNames(names)
         setTypes(types)
         setForms(forms)
+        setBootWithCar(BootWithCar)
       } catch (error) {
         console.error('Error fetching data:', error)
         toast.error(formatMessage({ id: 'toast.bootFetchingError' }))
@@ -137,7 +142,11 @@ export const EditBootDustCoverForm: React.FC<Props> = ({ data, className }) => {
 
         <div className='flex-1'>
           <div className='flex flex-col gap-5 mt-10'>
-            <EditBootModal open={openModal} onclose={() => setOpenModal(false)} />
+            <EditBootModal
+              open={openModal}
+              onclose={() => setOpenModal(false)}
+              cars={BootWithCar}
+            />
             <Button onClick={() => setOpenModal(true)} />
 
             <Title text='Связанные машины:' size='sm' className='font-bold' />
--- shared/components/shared/modals/edit-boot-modal/edit-boot-modal.tsx
+++ shared/components/shared/modals/edit-boot-modal/edit-boot-modal.tsx
@@ -1,26 +1,59 @@
+'use client'
+
+import { CarWithBoot, CarWithBrand } from '@/@types/prisma'
 import { Button } from '@/shared/components/ui'
 import { Dialog, DialogContent } from '@/shared/components/ui/dialog'
 import { cn } from '@/shared/lib/utils'
 import React from 'react'
+import { FormProvider, useForm } from 'react-hook-form'
+import { Container } from '../../container'
+import { FormDataTable } from '../../form'
+import { useIntl } from 'react-intl'
 
 interface Props {
+  cars: CarWithBrand[]
   open: boolean
   onclose: () => void
   className?: string
 }
 
-export const EditBootModal: React.FC<Props> = ({ open, onclose, className }) => {
+export const EditBootModal: React.FC<Props> = ({ open, onclose, cars, className }) => {
+  const { formatMessage } = useIntl()
+  const methods = useForm()
+
   const handleClose = () => {
     onclose()
   }
 
+  const columns = [
+    { key: 'carBrand.name', label: formatMessage({ id: 'bootCars.carBrand' }) },
+    { key: 'imageUrl', label: formatMessage({ id: 'bootCars.carImage' }) },
+    { key: 'models', label: formatMessage({ id: 'bootCars.carModel' }) },
+    { key: 'carBody', label: formatMessage({ id: 'bootCars.carBody' }) },
+    { key: 'modelYear', label: formatMessage({ id: 'bootCars.carYear' }) },
+    { key: 'engine', label: formatMessage({ id: 'bootCars.carEngine' }) },
+    { key: 'volume', label: formatMessage({ id: 'bootCars.carVolume' }) },
+    { key: 'actions', label: formatMessage({ id: 'bootCars.actions' }) },
+  ]
+
   return (
     <Dialog open={open} onOpenChange={handleClose}>
       <DialogContent
-        className={cn('w-[450px] p-10 bg-[#fcfcfc] dark:bg-zinc-900', className)}
+        className={cn('w-[650px] p-10 bg-[#fcfcfc] dark:bg-zinc-900', className)}
         aria-describedby={undefined}
       >
-        test
+        <FormProvider {...methods}>
+          <Container className='h-full flex flex-col py-5 secondary dark:bg-zinc-900'>
+            {cars && cars.length > 0 && (
+              <FormDataTable
+                name='cars'
+                label={formatMessage({ id: 'bootCars.carsList' })}
+                data={cars}
+                columns={columns}
+              />
+            )}
+          </Container>
+        </FormProvider>
       </DialogContent>
     </Dialog>
   )
