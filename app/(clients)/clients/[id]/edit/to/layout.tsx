e55eeffa56bcc7072205675ec12fabbfff2832f5
Igor Strelnyk <59284261+rodriguezz4060@users.noreply.github.com>
2024-10-15 11:09:16 +0300

    add back button

--- app/(clients)/clients/[id]/edit/to/layout.tsx
+++ app/(clients)/clients/[id]/edit/to/layout.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from 'next'
 import { Separator } from '@/shared/components/ui'
 import { SidebarNav } from '@/shared/components/shared/sidebar-nav'
+import { BackButton } from '@/shared/components/shared'
 
 export const metadata: Metadata = {
   title: 'Редактирование машины клиента | RodriguezDB',
@@ -27,12 +28,16 @@ export default function CarToEditLayout({ children, params: { id } }: SettingsLa
   return (
     <>
       <div className='hidden space-y-6 p-5 pb-16 md:block'>
-        <div className='space-y-0.5'>
-          <h2 className='text-2xl font-bold tracking-tight'>Редактирование</h2>
-          <p className='text-muted-foreground'>
-            Измените или заполните данные о ТО для машины клиента.
-          </p>
+        <div className='flex items-center justify-between'>
+          <div className='space-y-0.5'>
+            <h2 className='text-2xl font-bold tracking-tight'>Редактирование</h2>
+            <p className='text-muted-foreground'>
+              Измените или заполните данные о ТО для машины клиента.
+            </p>
+          </div>
+          <BackButton route='/clients/' id={id} />
         </div>
+
         <Separator className='my-6' />
         <div className='flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0'>
           <aside className='-mx-4 lg:w-1/5'>
