51ff526ac9803db7b1f110323ca3d3316dfb744b
Igor Strelnyk <rodriguezz4060@gmail.com>
2024-10-15 18:51:18 +0300

    del old component

--- shared/components/shared/clients/index.ts
+++ shared/components/shared/clients/index.ts
@@ -1,4 +1,3 @@
 export { ClientsPage } from './clients-page'
-export { ClientInfoPage } from './client'
 export { AddClientForm } from './add-client'
 export { ClientInfoBlock } from './client-info-block'
