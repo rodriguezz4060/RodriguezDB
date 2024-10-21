2ca17d7a48cae2c7b8722fba12e840d2d8f6d0e0
Igor Strelnyk <rodriguezz4060@gmail.com>
2024-10-21 17:45:33 +0300

    fix

--- app/(car-to)/car-to/hyundai/[id]/page.tsx
+++ app/(car-to)/car-to/hyundai/[id]/page.tsx
@@ -2,7 +2,7 @@ import { prisma } from '@/prisma/prisma-client'
 import { CarToInfoPage } from '@/shared/components/shared'
 import { notFound } from 'next/navigation'
 
-export default async function OilToyotaPage({ params: { id } }: { params: { id: string } }) {
+export default async function OilHyundaiPage({ params: { id } }: { params: { id: string } }) {
   const oilInfo = await prisma.oilCar.findFirst({
     where: { id: Number(id) },
     include: {
---
+++ app/(car-to)/car-to/nissan/page.tsx
@@ -0,0 +1,20 @@
+import { prisma } from '@/prisma/prisma-client'
+import { Container, CarCard } from '@/shared/components/shared'
+
+export default async function CarsNissanHome() {
+  const carBrandId = 1
+
+  const carsWithOilTo = await prisma.oilCar.findMany({
+    where: {
+      carBrandId: carBrandId,
+    },
+  })
+
+  return (
+    <>
+      <Container className='secondary dark:bg-zinc-900 px-4 pb-10 '>
+        <CarCard carTo={carsWithOilTo} rout='nissan' />
+      </Container>
+    </>
+  )
+}
