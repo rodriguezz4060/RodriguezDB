227e7962ba076bdb107fdef7f147dd7c883f4bb4
Igor Strelnyk <59284261+rodriguezz4060@users.noreply.github.com>
2024-10-21 14:37:27 +0300

    add pagination from boot page

--- shared/components/shared/boot-group-list.tsx
+++ shared/components/shared/boot-group-list.tsx
@@ -1,7 +1,9 @@
+'use client'
 import { cn } from '@/shared/lib/utils'
-import React from 'react'
+import React, { useState } from 'react'
 import { BootCard } from './boot-card'
 import { BootWithRelation } from '@/@types/prisma'
+import { PaginationComponent } from './pagination'
 
 interface Props {
   items: BootWithRelation[]
@@ -10,22 +12,44 @@ interface Props {
 }
 
 export const BootGroupList: React.FC<Props> = ({ items, listClassName, className }) => {
+  const [currentPage, setCurrentPage] = useState(1)
+  const itemsPerPage = 10
+
+  const totalPages = Math.ceil(items.length / itemsPerPage)
+
+  const paginatedBoot = items.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage)
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page)
+  }
+
   return (
     <div className={className}>
-      <div className={cn('grid grid-cols-3 gap-[50px]', listClassName)}>
-        {items.map((product, i) => (
-          <BootCard
-            key={product.id}
-            id={product.id}
-            name={product.name.name}
-            type={product.type}
-            imageUrl={product.imageUrl ?? ''}
-            dIn={product.dIn}
-            dOut={product.dOut}
-            height={product.height}
-            partNumber={product.partNumber}
+      <div className='flex flex-col min-h-screen'>
+        <div className='flex-grow'>
+          <div className={cn('grid grid-cols-3 gap-[50px]', listClassName)}>
+            {paginatedBoot.map((product, i) => (
+              <BootCard
+                key={product.id}
+                id={product.id}
+                name={product.name.name}
+                type={product.type}
+                imageUrl={product.imageUrl ?? ''}
+                dIn={product.dIn}
+                dOut={product.dOut}
+                height={product.height}
+                partNumber={product.partNumber}
+              />
+            ))}
+          </div>
+        </div>
+        <div className='mt-5'>
+          <PaginationComponent
+            currentPage={currentPage}
+            totalPages={totalPages}
+            onPageChange={handlePageChange}
           />
-        ))}
+        </div>
       </div>
     </div>
   )
